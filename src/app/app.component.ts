e486126087b762dd90a7012281269dcbc1f569d2
GRDtechlab <grdtechlab@gmail.com>
2022-04-06 14:54:22 +0530

    final lint eslint prettier working updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = "husky"; 
-AVc = "a"   
+  title = 'husky';
 
+  AVc = 'a';
 
+  @Input() count = 0;
 
+  public run() {
+    debugger;
+  }
 }
