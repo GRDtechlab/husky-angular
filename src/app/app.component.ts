1952b2fcbe7aca36c524293695070452889cc8fe
GRDtechlab <grdtechlab@gmail.com>
2022-04-07 12:18:34 +0530

    feat: commit format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'husky';
 
   AVc = 'a';
 
-  @Input() count = 0;
+  @Input('test') count = 0;
+
+  ngOnInit() {
+    let abc = 0;
+    console.log({ abc });
+  }
 
   public run() {
     debugger;
